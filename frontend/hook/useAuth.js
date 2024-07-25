943dc5aca84f379fe85ef3a3a5bfb39278120f53
juancastp <juancastp@gmail.com>
2024-07-25 10:52:51 +0200

    useAuth.js

---
+++ frontend/hook/useAuth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import axiosInstance from '../api/axiosConfig';
+
+export const useAuth = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await axiosInstance.get('/user');
+        setUser(response.data);
+        setIsAuthenticated(true);
+      } catch (error) {
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  return { isAuthenticated, user };
+};
