adec2ee9c959d3bdc209c358eab9e6764a684c4b
juancastp <juancastp@gmail.com>
2024-07-26 12:35:48 +0200

    auth

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,10 +2,20 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.51.172:8000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'http://192.168.51.172:8000/api', // Reemplaza con tu URL base
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // Asegúrate de que el token se almacene en localStorage al iniciar sesión
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
